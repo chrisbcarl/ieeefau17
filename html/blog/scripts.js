584861fd80fa459df7dcb2bb33dfdcc7a94d6f3e
Chris Carl <chrisbcarl@outlook.com>
2017-04-02 12:06:12 -0400

    version 3

--- html/blog/scripts.js
+++ html/blog/scripts.js
@@ -1,80 +1,62 @@
 $(document).ready(function(){
-    console.log("got here");
-    $("#ieeelogo").click(function(){
-        console.log("ieeelogo" + " clicked");
-    });
-
-    $("#ieeefau").click(function(){
-        console.log("ieeefau" + " clicked");
-    });
-
-
+    console.log("got to load blog doc ready");
 
     //lndex
-    console.log("started the song and dance");
+    console.log("we out here bb");
     forcecontent("html/default.html");
-    console.log("forced content through");
-
-    //about
-    loadcontent("#whoweare", "html/about/whoweare.html");
-    loadcontent("#leadership", "html/about/leadership.html");
-    loadcontent("#members", "html/about/members.html");
+    console.log("forced content through lol");
 
     //blog
     loadcontent("#toc", "html/blog/toc.html");
-    loadcontent("#categories", "html/blog/categories.html");
-    loadcontent("#authors", "html/blog/authors.html");
 
     //blog posts
-    loadblog("#aprilvolunteer", "html/blog/posts/aprilvolunteer.html", "html/blog/authors/raldirisr.html");
-    loadblog("#ilead", "html/blog/posts/ilead.html", "html/blog/authors/moralese.html");
-    loadblog("#isense", "html/blog/posts/isense.html", "html/blog/authors/raldirisr.html");
-    loadblog("#roboticsminutes", "html/blog/posts/roboticsminutes.html", "html/blog/authors/garciaw.html");
-    loadblog("#whyieee", "html/blog/posts/whyieee.html", "html/blog/authors/phillips.html");
-
-    //events
-    loadcontent("#eweek", "html/events/eweek.html");
-    loadcontent("#bluemix", "html/events/bluemix.html");
-    loadcontent("#southeastcon", "html/events/southeastcon.html");
-    loadcontent("#meeting", "html/events/meeting.html");
+    $(document).on("click", "#aprilvolunteer",  {elem: "#aprilvolunteer",   pagedir: "html/blog/posts/aprilvolunteer.html",     authordir: "html/blog/authors/raldirisr.html"}, loadblog);
+    $(document).on("click", "#roboticsminutes", {elem: "#roboticsminutes",  pagedir: "html/blog/posts/roboticsminutes.html",    authordir: "html/blog/authors/garciaw.html"}, loadblog)
+    $(document).on("click", "#ilead",           {elem: "#ilead",            pagedir: "html/blog/posts/ilead.html",              authordir: "html/blog/authors/moralese.html"}, loadblog);
+    $(document).on("click", "#isense",          {elem: "#isense",           pagedir: "html/blog/posts/isense.html",             authordir: "html/blog/authors/raldirisr.html"}, loadblog);
+    $(document).on("click", "#whyieee",         {elem: "#whyieee",          pagedir: "html/blog/posts/whyieee.html",            authordir: "html/blog/authors/phillipsj.html"}, loadblog);
+    $(document).on("click", "#argonlasertag",   {elem: "#argonlasertag",    pagedir: "html/blog/posts/argonlasertag.html",      authordir: "html/blog/authors/phillipsj.html"}, loadblog);
 });
 
 function toggle_bg_off() { $("html").removeClass("full").addClass("full-no-image"); }
 function toggle_bg_on() { $("html").removeClass("full-no-image").addClass("full"); }
-function loadhome(elem)
-{
-    $(elem).click(function(){
-        console.log(elem + " clicked");
-        $("#content").hide();
-        //toggle_bg_on();
-    });
-}
-function loadcontent(elem, pagedir)
-{
+
+function loadcontent(elem, pagedir){
     $(elem).click(function(){
         console.log(elem + " clicked");
         $("#content").load(pagedir);
         $("#content").show();
         //toggle_bg_off();
-        $(".dropdown-toggle").dropdown("toggle");
+        $(".dropdown-menu li a").removeClass("selected");
+        $(elem).addClass("selected");
+        if ($(elem).is("a")) {
+            $(".navbar-collapse").collapse("hide");
+        }
+        $(".dropdown-toggle").blur()
     });
 }
-function forcecontent(pagedir)
-{
+
+function forcecontent(pagedir){
     console.log("force loaded content from: " + pagedir);
     $("#content").load(pagedir);
     $("#content").show();
     //toggle_bg_off();
 }
-function loadblog(elem, pagedir, authordir)
-{
-    console.log("loading blog");
-    $(elem).click(function(){
-        console.log(elem + " clicked");
-        $("#content").load(pagedir);
-        $("#content").show();
-        $("#author").load(authordir);
-        //toggle_bg_off();
-        $(".dropdown-toggle").dropdown("toggle");
-    });
+
+function loadblog(event){
+    // console.log("loading blog");
+    console.log(event.data.elem + " clicked");
+    $("#content").load(event.data.pagedir);
+    $("#content").show();
+    $("#author").load(event.data.authordir);
+    $("#author").show();
+    $("#toc").load("toc.html");
+    $("#toc").show();
+    //toggle_bg_off();
+    $(".dropdown-menu li a").removeClass("selected");
+    $(event.data.elem).addClass("selected");
+    if ($(event.data.elem).is("a")) {
+        $(".navbar-collapse").collapse("hide");
+    }
+    $(".dropdown-toggle").blur()
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,50 @@
 $(document).ready(function(){
-    console.log("got here");
-    $("#ieeelogo").click(function(){
-        console.log("ieeelogo" + " clicked");
-    });
-
-    $("#ieeefau").click(function(){
-        console.log("ieeefau" + " clicked");
-    });
-
-    setInterval(app(), 3000);
-
     //lndex
     console.log("started the song and dance");
-    forcecontent("html/default.html");
+    loadhome();
+    //forcecontent("html/default.html", "html/header.html");
     console.log("forced content through");
 
+    app();
 });
 
-function loadcontent(elem, pagedir)
-{
+function app(){
+
+    //index
+    $("#ieeelogo").click(function(){
+        console.log("ieeelogo clicked");
+        loadhome();
+    });
+
+    //about
+    loadcontent("#whoweare", "html/about/whoweare.html");
+    loadcontent("#leadership", "html/about/leadership.html");
+    loadcontent("#members", "html/about/members.html");
+
+    //blog
+    loadcontent("#toc", "html/blog/toc.html");
+
+    //blog posts
+    $(document).on("click", "#aprilvolunteer",  {elem: "#aprilvolunteer",   pagedir: "html/blog/posts/aprilvolunteer.html",     authordir: "html/blog/authors/raldirisr.html"}, loadblog);
+    $(document).on("click", "#roboticsminutes", {elem: "#roboticsminutes",  pagedir: "html/blog/posts/roboticsminutes.html",    authordir: "html/blog/authors/garciaw.html"}, loadblog)
+    $(document).on("click", "#ilead",           {elem: "#ilead",            pagedir: "html/blog/posts/ilead.html",              authordir: "html/blog/authors/moralese.html"}, loadblog);
+    $(document).on("click", "#isense",          {elem: "#isense",           pagedir: "html/blog/posts/isense.html",             authordir: "html/blog/authors/raldirisr.html"}, loadblog);
+    $(document).on("click", "#whyieee",         {elem: "#whyieee",          pagedir: "html/blog/posts/whyieee.html",            authordir: "html/blog/authors/phillipsj.html"}, loadblog);
+    $(document).on("click", "#argonlasertag",   {elem: "#argonlasertag",    pagedir: "html/blog/posts/argonlasertag.html",      authordir: "html/blog/authors/phillipsj.html"}, loadblog);
+
+    //events
+    loadcontent("#eweek",           "html/events/eweek.html");
+    loadcontent("#bluemix",         "html/events/bluemix.html");
+    loadcontent("#southeastcon",    "html/events/southeastcon.html");
+    loadcontent("#meeting",         "html/events/meeting.html");
+}
+
+function toggle_bg_off() { $("html").removeClass("full").addClass("full-no-image"); }
+function toggle_bg_on() { $("html").removeClass("full-no-image").addClass("full"); }
+function loadcontent(elem, pagedir){
     $(elem).click(function(){
         console.log(elem + " clicked");
+        $("#header").hide();
         $("#content").load(pagedir);
         $("#content").show();
         //toggle_bg_off();
@@ -33,81 +57,43 @@ function loadcontent(elem, pagedir)
         $(".dropdown-toggle").blur()
     });
 }
-
-
-function app()
-{
-
-        //about
-        loadcontent("#whoweare", "html/about/whoweare.html");
-        loadcontent("#leadership", "html/about/leadership.html");
-        loadcontent("#members", "html/about/members.html");
-
-        //blog
-        loadcontent("#toc", "html/blog/toc.html");
-        loadcontent("#categories", "html/blog/categories.html");
-        loadcontent("#authors", "html/blog/authors.html");
-
-        //blog posts
-        $("#aprilvolunteer").click(function(){
-            console.log("#aprilvolunteer" + " clicked");
-            $("#content").hide();
-            $("#content").load("html/blog/posts/aprilvolunteer.html");
-            $("#content").show();
-            $("#author_section").load("html/blog/authors/raldirisr.html");
-            //toggle_bg_off();
-            $(".dropdown-toggle").dropdown("toggle");
-        });
-    /*
-        loadblog("#aprilvolunteer", "html/blog/posts/aprilvolunteer.html", "html/blog/authors/raldirisr.html");
-        loadblog("#ilead", "html/blog/posts/ilead.html", "html/blog/authors/moralese.html");
-        loadblog("#isense", "html/blog/posts/isense.html", "html/blog/authors/raldirisr.html");
-        loadblog("#roboticsminutes", "html/blog/posts/roboticsminutes.html", "html/blog/authors/garciaw.html");
-        loadblog("#whyieee", "html/blog/posts/whyieee.html", "html/blog/authors/phillips.html");
-    */
-
-        //events
-        loadcontent("#eweek", "html/events/eweek.html");
-        loadcontent("#bluemix", "html/events/bluemix.html");
-        loadcontent("#southeastcon", "html/events/southeastcon.html");
-        loadcontent("#meeting", "html/events/meeting.html");
+function loadhome(){
+    console.log("we made it bois")
+    //$("#header").load("html/header.html");
+    $("#header").show();
+    $("#content").load("html/default.html");
+    $("#content").show();
+    $(".dropdown-menu li a").removeClass("selected");
 }
-function toggle_bg_off() { $("html").removeClass("full").addClass("full-no-image"); }
-function toggle_bg_on() { $("html").removeClass("full-no-image").addClass("full"); }
-function loadhome(elem)
-{
-    $(elem).click(function(){
-        console.log(elem + " clicked");
-        $("#content").hide();
-        //toggle_bg_on();
-    });
-}
-// function loadcontent(elem, pagedir)
-// {
-//     $(elem).click(function(){
-//         console.log(elem + " clicked");
-//         $("#content").load(pagedir);
-//         $("#content").show();
-//         //toggle_bg_off();
-//         $(".dropdown-toggle").dropdown("toggle");
-//     });
-// }
-function forcecontent(pagedir)
-{
+function forcecontent(pagedir){
     console.log("force loaded content from: " + pagedir);
     $("#content").load(pagedir);
     $("#content").show();
     //toggle_bg_off();
 }
-function loadblog(elem, pagedir, authordir)
-{
-    console.log("loading blog");
-    $(elem).click(function(){
-        console.log(elem + " clicked");
-        $("#content").load(pagedir);
-        $("#content").show();
-        $("#author_section").load(authordir);
-        //toggle_bg_off();
-        $(".dropdown-toggle").dropdown("toggle");
-    });
+function forcecontent(dir1, dir2){
+    console.log("force loaded content from: " + dir1 + dir2);
+    //$("#header").load(dir2);
+    $("#header").show();
+    $("#content").load(dir1);
+    $("#content").show();
+    //toggle_bg_off();
+}
+function loadblog(event){
+    // console.log("loading blog");
+    console.log(event.data.elem + " clicked");
+    $("#header").hide();
+    $("#content").load(event.data.pagedir);
+    $("#content").show();
+    $("#author").load(event.data.authordir);
+    $("#author").show();
+    $("#toc").load("toc.html");
+    $("#toc").show();
+    //toggle_bg_off();
+    $(".dropdown-menu li a").removeClass("selected");
+    $(event.data.elem).addClass("selected");
+    if ($(event.data.elem).is("a")) {
+        $(".navbar-collapse").collapse("hide");
+    }
+    $(".dropdown-toggle").blur()
 }
